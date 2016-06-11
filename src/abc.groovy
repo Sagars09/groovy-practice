074713a7d795a3dcbaf96b75937a40bf0a084b21
Sagar Jain <jain.jains.sagar@gmail.com>
2016-06-11 09:43:33 +0530

    Modified abc.groovy which containspushing of items and some operators.

--- src/abc.groovy
+++ src/abc.groovy
@@ -13,11 +13,11 @@ class User1 {
     }
 }
 
-Map m1 = [firstName: "Sagar", lastName: "Jain", email: "jain", age: 5]
-Map m2 = [firstName: "Shubham", lastName: "Jain", email: "jain", age: 4]
-Map m3 = [firstName: "Priyal", lastName: "Singh", email: "jain", age: 3]
-Map m4 = [firstName: "Smita", lastName: "P", email: "jain", age: 2]
-Map m5 = [firstName: "Akshita", lastName: "Gangarde", email: "jain", age: 1]
+Map m1 = [firstName: "Sagar", lastName: "Jain", email: "jain", age: 500]
+Map m2 = [firstName: "Shubham", lastName: "Jain", email: "jain", age: 40]
+Map m3 = [firstName: "Priyal", lastName: "Singh", email: "jain", age: 300]
+Map m4 = [firstName: "Smita", lastName: "P", email: "jain", age: 20]
+Map m5 = [firstName: "Akshita", lastName: "Gangarde", email: "jain", age: 100]
 
 User1 u1 = new User1(m1)
 User1 u2 = new User1(m2)
@@ -26,9 +26,23 @@ User1 u4 = new User1(m4)
 User1 u5 = new User1(m5)
 
 List l = [u1,u2,u3,u4,u5]
-
-l.each {
+List m = []
+List n = []
+l.each { item->
  //   println "${it.firstName} ${it.lastName} (${it.age})"
-    println it
-}
+    println item
+    if (item.age >= 100) {
+        m.push(item)
+    }
 
+    if (item.age < 100) {
+        n.push(item)
+    }
+
+}
+println m
+println n
+
+/* List m = l.findAll {
+    it.age >100 }
+ */
